106a667ce350657256b03b92b95cbc1ddd52dbe0
cSchwarz117 <38546318+cSchwarz117@users.noreply.github.com>
2019-07-14 12:10:06 -0700

    added some pickle functionality

--- client/client.py
+++ client/client.py
@@ -1,14 +1,27 @@
 #Code goes here
 
 import socket
+import pickle
 
 HOST = '127.0.0.1' # The server's hostname of IP address
 PORT = 65432       #  The port used by the server
 
+usr = input('Please enter your Username: ')
+pwd = input('Please enter your Password: ')
+
+cred = {"username": usr, "password": pwd}
+#outfile = open('cred', 'wb')
+#pickle.dump(cred, outfile)
+#outfile.close()
+
+out = pickle.dumps(cred)
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    s.sendall(b'Hello, world')
+    s.sendall(out)
     data = s.recv(1024)
+
+data = pickle.loads(data)
 
 print('Recieved', repr(data))
 
